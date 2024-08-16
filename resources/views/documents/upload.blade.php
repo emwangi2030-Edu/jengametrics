bc5c8daa7135098a7196c6c08f38038921e858d7
Ezekiel Muki <ezekielmuki42@gmail.com>
2024-08-16 11:14:10 +0300

    template updates

--- resources/views/documents/upload.blade.php
+++ resources/views/documents/upload.blade.php
@@ -1,14 +1,5 @@
-<!-- resources/views/documents/upload.blade.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload Document</title>
-    <!-- Include Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+@extends('layouts.appbar')
+@section('content') 
     <div class="container mt-5">
         <h1 class="mb-4">Upload Document</h1>
 
@@ -58,5 +49,5 @@
 
     <!-- Include Bootstrap JS (Optional) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+
+@endsection
