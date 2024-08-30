0d531f5e925f216ecbbbcd077c8ebbb67351ebc8
NKGachoki <njambagachoki@gmail.com>
2024-08-30 13:25:03 +0300

    some changes to appbar and materials

--- resources/views/layouts/appbar.blade.php
+++ resources/views/layouts/appbar.blade.php
@@ -37,7 +37,6 @@ License: For each use you must have a valid license purchased only from above li
     <link href="{{ asset('assets') }}/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css">
     <!--end::Vendor Stylesheets-->
 
-
     <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
     <link href="{{ asset('assets') }}/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets') }}/css/style.bundle.css" rel="stylesheet" type="text/css">
