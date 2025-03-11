7a9bc7fa40ad0d2860862c69d09515fe877896fc
NKGachoki <njambagachoki@gmail.com>
2025-03-11 14:04:23 +0300

    fixed registration

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -52,8 +52,6 @@
       
 </div>
 
-<!-- Bootstrap 5 JS Bundle -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Chart.js for Graphs -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
